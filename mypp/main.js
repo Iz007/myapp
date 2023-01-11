2b0d88adb2d250d2e5eab885a5d859ec744b8e63
Iz007 <122432042+Iz007@users.noreply.github.com>
2023-01-11 14:44:00 +0700

    Add files via upload

--- mypp/main.js
+++ mypp/main.js
@@ -1,38 +1,35 @@
-function.getElementById("Calculate").addEventListener("click",function()
-{
-    var num1 = this.document.getElementById("num1").value;
-    var num2 = this.document.getElementById("num2").value;
-    var sum = parseInt(num1) + parseInt(num2);
-    document.getElementById("result").innerHTML = "S" +sum;
-});
-function.getElementById("AA").addEventListener("click",function()
-{
-    var num1 = this.document.getElementById("num1").value;
-    var num2 = this.document.getElementById("num2").value;
-    var sum = parseInt(num1) - parseInt(num2);
-    document.getElementById("result").innerHTML = "S" +sum;
-});
-function.getElementById("C").addEventListener("click",function()
-{
-    var num1 = this.document.getElementById("num1").value;
-    var num2 = this.document.getElementById("num2").value;
-    var sum = parseInt(num1) * parseInt(num2);
-    document.getElementById("result").innerHTML = "S" +sum;
-});
-function.getElementById("T").addEventListener("click",function()
-{
-    var num1 = this.document.getElementById("num1").value;
-    var num2 = this.document.getElementById("num2").value;
-    var sum = parseInt(num1) / parseInt(num2);
-    document.getElementById("result").innerHTML = "S" +sum;
-});
-
-function myalert(){
-    Alert(olo)
-
-}
 
 function Calculate1(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1) + parseInt(num2);
+    alert(innerHTML = "SUM : " +sum);
+};
+
+function Calculate2(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1) - parseInt(num2);
+    alert(innerHTML = "SUM : " +sum);
+};
+v
+
+function Calculate3(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1) * parseInt(num2);
+    alert(innerHTML = "SUM : " +sum);
+};
+
+
+function Calculate4(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1) / parseInt(num2);
+    alert(innerHTML = "SUM : " +sum);
+};
+
+function myalert(){
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
     var sum = parseInt(num1) + parseInt(num2);
